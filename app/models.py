62ddb3cc9959d799f6f5b4a4e33eb23b7499f723
GustavoKobielski <gust.kobielski@gmail.com>
2024-07-30 10:58:29 -0300

    Sua mensagem de commit

--- app/models.py
+++ app/models.py
@@ -1,1 +1,22 @@
-from app import db
+from app import db, login_manager
+from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=False,default="Individuo Indigente")
+    email = db.Column(db.String, nullable=False)
+    senha = db.Column(db.String, nullable=False)
+    adm = db.Column(db.Boolean, default=False)
+
+
+
+
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,1 +1,41 @@
 from app import app, db
+from flask import render_template, url_for, request, redirect, jsonify
+from flask_login import login_user, logout_user, current_user, login_required
+from app.forms import LoginForm, UserForm
+
+
+#Pagina Inicial
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    usuario = 'Mateus'
+    idade = 17
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = form.login()
+        login_user(user, remember=True)
+        return redirect(url_for('homepage'))
+
+    context = {
+        'usuario': usuario,
+        'idade': idade
+    }
+    return render_template('index.html', context=context, form=form)
+
+#Função de Cadastro
+@app.route('/cadastro/', methods=['GET', 'POST'])
+def cadastro():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = form.save()
+        login_user(user, remember=True)
+        return redirect(url_for('homepage'))
+    return render_template('cadastro.html', form=form)
+
+# Função de dar logout
+@app.route('/sair/')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
