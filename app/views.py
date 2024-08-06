ce399df5aeaccf847192a1fa80529a8d98e01eda
Felipe G <gabrielfelipewroblewski@gmail.com>
2024-08-06 10:51:45 -0300

    paginaEntrar

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app, db
 from flask import render_template, url_for, request, redirect, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm, UserForm
+from app.models import User
 
 
 #Pagina Inicial
@@ -39,3 +40,62 @@ def cadastro():
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
+
+#############################################
+######## GMAIL SETUP ###############
+#############################################
+
+from authlib.integrations.flask_client import OAuth
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id='841818364820-eg98vp2chajm8vfnuku3tvsj5s91egbb.apps.googleusercontent.com',
+    client_secret='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',
+    authorize_params=None,
+    access_token_params=None,
+    refresh_token_url=None,
+    refresh_token_params=None,
+    redirect_uri='http://localhost:5000/login/google/callback',
+    client_kwargs={'scope': 'openid profile email'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'  # URL dos metadados do servidor do Google OAuth
+)
+
+@app.route('/login/google')
+def login_google():
+    redirect_uri = url_for('authorize_google', _external=True)
+    return google.authorize_redirect(redirect_uri, prompt='select_account')
+
+
+
+@app.route('/login/google/callback')
+def authorize_google():
+    token = google.authorize_access_token()
+    print("Token de Acesso do Google:", token)
+
+    # Verifique se o token foi obtido corretamente
+    if 'access_token' in token:
+        # Faça uma solicitação para obter as informações do usuário usando o token de acesso
+        resp = google.get('https://www.googleapis.com/oauth2/v1/userinfo', token=token)
+        user_info = resp.json()
+
+        # Verifique se as informações do usuário foram obtidas corretamente
+        if user_info:
+            # Verifique se o usuário já existe no banco de dados com base no e-mail fornecido pelo Google
+            user = User.query.filter_by(email=user_info['email']).first()
+
+            if not user:
+                # Se o usuário não existir, crie um novo registro no banco de dados
+                user = User(nome=user_info.get('given_name', ''),
+                            sobrenome=user_info.get('family_name', ''),
+                            email=user_info.get('email', ''))
+                db.session.add(user)
+                db.session.commit()
+
+            # Faça login do usuário
+            login_user(user, remember=True)
+
+            # Redirecione o usuário para a página principal
+            return redirect(url_for('homepage'))
+
+    # Se houver algum problema, redirecione o usuário para uma página de erro ou para a página de login novamente
+    return redirect(url_for('login_google'))
